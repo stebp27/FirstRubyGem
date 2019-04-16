1b880c9e522964ed1a24252fe697895986ec08a9
Stefano Beltrami <root@stebp27-VirtualBox>
2019-04-16 16:05:11 +0200

    Finish working and refactored palindrome method

--- lib/stebp27_palindrome.rb
+++ lib/stebp27_palindrome.rb
@@ -1,6 +1,19 @@
 require "stebp27_palindrome/version"
 
-module Stebp27Palindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+
+	def palindrome?
+		processed_content == processed_content.reverse
+	end
+
+	def letters
+		self.scan(/[a-z]/i).join.downcase
+	end
+
+	private
+
+	def processed_content
+		self.letters.downcase
+	end
 end
